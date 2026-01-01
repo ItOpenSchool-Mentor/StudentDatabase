f54cb77e2ff3698bb192d4683b5dfb8b14066cad
StudentDatabase <itopenschool2024@gmail.com>
2026-01-01 21:55:47 +0530

    IInd Update

---
+++ day-5/scr1.py
@@ -0,0 +1,12 @@
+# print("Hi", end="@#&*")
+# print("World!")
+
+# a = 5
+# b = 10
+# print(a, b, sep="---")
+
+a = 5
+b = 10
+print(end="+")
+print("Hello", "World", sep="+-+", end="*")
+print(a, b, sep="", end="<>")
