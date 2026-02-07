624a8c9e3298712486a53638bc31ac16b92e6a21
StudentDatabase <itopenschool2024@gmail.com>
2026-02-07 16:25:14 +0530

    Added day-12

---
+++ day-12/scr1.py
@@ -0,0 +1,16 @@
+# Comparison Operators
+##a = 10
+##b = 5
+##print(a > b)
+
+#print(5 == 5.0) #TRue
+
+#print("apple" > "banana") #False 
+##print("ape" == "apes") #False
+##print("ape" < "apes") #True
+
+
+x = 10
+
+print(5 < x < 20 > 50)  
+print(5 < x < 20 < 50)  
---
+++ day-12/scr2.py
@@ -0,0 +1,17 @@
+##print(10 and 20) # 20 
+##print(0 and 20) # 0
+##print(0 or 30) # 30
+##print(10 or 20) # 10
+## 
+
+
+##print("" or "Python") # "Python"
+##print([] and 10) # []
+
+##and = First False or last val
+##or = First True
+
+print("" or 0 or "Python" or "Hi") # "Python"
+print(20 and 50 and 30) # 30
+print(20 and 50 and 0 and 100) # 0
+
