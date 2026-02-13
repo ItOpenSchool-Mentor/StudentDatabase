2f3362b7c0eb4e5e1ff52f09a330a9c588773e33
StudentDatabase <itopenschool2024@gmail.com>
2026-02-13 18:29:22 +0530

    Added day-14

---
+++ day-14/scr1.py
@@ -0,0 +1,23 @@
+# 1. Bitwise AND &
+a = 5
+b = 3
+
+print(a & b)
+
+#2. Bitwise OR |
+print(a | b)
+
+#3. Bitwise XOR ^
+print(a ^ b)
+
+#4 Bitwise NOT ~
+print(~5)
+print(~3)
+
+#5 Bitwise Left Shift <<
+print(a << 2)  # a * 2^2
+
+#6 Right Shift >>
+print(a >>1)  # a // 2^1
+
+
